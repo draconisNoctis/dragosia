3a40dd72a41d9d907fb3cd7920d5845ff0dc9f57
Mark Wecke <wecke@neoskop.de>
2019-01-05 16:30:20 +0100

    chore: remove gh-deploy script in favor to angular-cli-ghpages

--- bin/gh-pages.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-(cd modules/angular && yarn ng build char-sheet -c de-gh-pages)
-
-git add modules/angular/dist -f
-git commit -m "build"
-git push --delete origin gh-pages
-git subtree push --prefix modules/angular/dist/apps/char-sheet origin gh-pages
-git reset --hard HEAD^1
